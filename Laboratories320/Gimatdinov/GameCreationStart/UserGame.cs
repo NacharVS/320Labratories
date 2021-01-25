0310ff2b0285a2f669ef76f628bdfd461c99e3f2
keston616 <airat.shigapov2015@yandex.ru>
2021-01-25 09:28:32 +0300

    EndLesson(25.01.21)

---
+++ Laboratories320/Gimatdinov/GameCreationStart/UserGame.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Laboratories320.Shigapov.GameCreationStart
+{
+    class UserGame
+    {
+        public string Name { get; set; }
+        public DateTime DayOfBirth { get; set; }
+        public int FinishedMatches { get; set; }
+        public int Victories { get; set; }
+        public int GamePoints { get; set; }
+
+        public UserGame(string name, DateTime BirthDate,int finishedMatches, int victories, int gamePoints)
+        {
+            this.Name = name;
+            this.DayOfBirth = BirthDate;
+            this.FinishedMatches = finishedMatches;
+            this.Victories = victories;
+            this.GamePoints = gamePoints;
+        }
+
+        public int GetStat()
+        {
+            return FinishedMatches;
+        }
+
+    }
+}
