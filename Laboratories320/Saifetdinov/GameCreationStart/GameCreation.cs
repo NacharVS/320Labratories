dde951d83f0781f0e1e7b95bdcf74cfbb406bb5d
Эмиль <77325950+ImilSaifetdinov@users.noreply.github.com>
2021-01-25 17:46:12 +0300

    Finish prac6

--- Laboratories320/Saifetdinov/GameCreationStart/GameCreation.cs
+++ Laboratories320/Saifetdinov/GameCreationStart/GameCreation.cs
@@ -1,41 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
 
 namespace Laboratories320.Saifetdinov.GameCreationStart
 {
-    class GameCreation
+    public class GameCreation
     {
+        public int Id { get; set; }
         public string UserName { get; set; }
         public string DayOfBirth { get; set; }
         public int FinishedMatches { get; set; }
         public int Victories { get; set; }
         public int GamePoints { get; set; }
 
-        public GameCreation(string userName, string dayOfBirth, int finishedMatchees, int victories, int gamePoints)
+        public GameCreation(int id, string userName, string dayOfBirth, int finishedMatchees, int victories, int gamePoints)
         {
+            this.Id = id;
             this.UserName = userName;
             this.DayOfBirth = dayOfBirth;
             this.FinishedMatches = finishedMatchees;
             this.Victories = victories;
             this.GamePoints = gamePoints;
         }
-        
-    }
-    class Player
-    {
-        GameCreation player1 = new GameCreation("Джордж", "15-02-2002", 2, 1, 500);
-        GameCreation player2 = new GameCreation("Коржика", "14-08-2001", 5, 3, 1000);
-        GameCreation player3 = new GameCreation("Зрек", "20-25-2006", 7, 4, 1500);
-        GameCreation player4 = new GameCreation("Соник", "01-05-2004", 1, 0, 10);
-        GameCreation player5 = new GameCreation("Оскар", "03-07-2005", 6, 2, 140);
-    }
-
-    class Inplement
-    {
-        public static void SearchByName()
+        public void GetAName()
         {
-
+            Console.WriteLine($"Id player - {Id} name player - {UserName}");
+        }
+        public void StatisticsGame()
+        {
+            Console.WriteLine($"Id player - {Id}, FinishedMatches - {FinishedMatches}, Victories - {Victories}, Game Points - {GamePoints}");
         }
     }
+ 
 }
---
+++ Laboratories320/Saifetdinov/GameCreationStart/PlayersGame.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratories320.Saifetdinov.GameCreationStart
+{
+    class PlayersGame
+    {
+            public static GameCreation player1 = new GameCreation(1, "Джордж", "15-02-2002", 2, 1, 500);
+            public static GameCreation player2 = new GameCreation(2, "Коржика", "14-08-2001", 5, 3, 1000);
+            public static GameCreation player3 = new GameCreation(3, "Зрек", "20-25-2006", 7, 4, 1500);
+            public static GameCreation player4 = new GameCreation(4, "Соник", "01-05-2004", 1, 0, 10);
+            public static GameCreation player5 = new GameCreation(5, "Оскар", "03-07-2005", 6, 2, 140);
+
+            public static List<GameCreation> players = new List<GameCreation> { player1, player2, player3, player4, player5 };
+
+            public static void SearchByName()
+            {
+                Parallel.ForEach(players, s => s.GetAName());
+            }
+            public static void ReturnAllStats()
+            {
+                Parallel.ForEach(players, s => s.StatisticsGame());
+            }
+
+    }
+}
