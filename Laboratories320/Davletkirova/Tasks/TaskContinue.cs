cd7088fbbddecde155c3381c9f37d772d681aa12
Chergionva <daryacherginova19@mail.ru>
2021-01-25 09:11:57 +0300

    finish 4 practical work

---
+++ Laboratories320/Davletkirova/Tasks/TaskContinue.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratories320.Davletkirova.Tasks
+{
+    class TaskContinue
+    {
+        public static void Task()
+        {
+            Task task1 = new Task(GenerationElementsArray);
+            Task task2 = task1.ContinueWith(task1 => Multication());
+            Task task3 = task2.ContinueWith(task2 => ParityNumber());
+            task1.Start();
+            task2.Wait();
+            task3.Wait();
+            Console.ReadKey();
+
+        }
+        static int[] array = new int[20];
+        static int multiplication = 1;
+        public static void GenerationElementsArray()
+        {
+            Console.Write("Array - ");
+            Random random = new Random();
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = random.Next(1, 4);
+                Console.Write(array[i]);
+            }
+            Console.WriteLine();
+        }
+        public static void Multication()
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                multiplication *= array[i];
+            }
+            Console.WriteLine($"Multiplication - {multiplication}");
+        }
+        public static void ParityNumber()
+        {
+            Console.Write("Parity Number - ");
+            int number;
+            while (multiplication > 0)
+            {
+                number = multiplication % 10;
+                if (number % 2 == 0)
+                {
+                    Console.Write($"{number} ");
+                }
+                multiplication /= 10;
+            }
+        }
+    }
+}
---
+++ Laboratories320/Davletkirova/ThreadLocker/ThreadLocker.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace Laboratories320.Mazitova.Pr3
+{
+    class ThreadLocker
+    {
+        static int[] arr1 = new int[10];
+        static int[] arr2 = new int[10];
+        static int[] arr3 = new int[10];
+
+        static object locker = new object();
+        public void Start()
+        {
+            Thread GenThr = new Thread(GetArray);
+            Thread SumThr = new Thread(Sum);
+
+            GenThr.Start();
+            Thread.Sleep(1000);
+            SumThr.Start();
+            Thread.Sleep(2000);
+
+        }
+        public void GetArray()
+        {
+            lock (locker)
+            {
+                Console.WriteLine("First array");
+                Random random = new Random();
+                for (int i = 0; i < arr1.Length; i++)
+                {
+                    arr1[i] = random.Next(-100, 100);
+                }
+
+                foreach (var it in arr1)
+                {
+                    Console.WriteLine(it + " ");
+                }
+            }
+            lock (locker)
+            {
+                
+                Console.WriteLine("Second array");
+                Random random = new Random();
+                for (int i = 0; i < arr2.Length; i++)
+                {
+                    arr2[i] = random.Next(-100, 100);
+                }
+
+                foreach (var it in arr2)
+                {
+                    Console.WriteLine(it + " ");
+                }
+            }
+
+        }
+        
+
+        public void Sum()
+        {
+            lock (locker)
+            {
+                
+                Console.WriteLine("Sum arrays");
+                for (int i = 0; i < arr1.Length && i < arr2.Length; i++)
+                {
+                    arr3[i] = arr1[1] + arr2[i];
+                }
+
+                foreach (var item in arr3)
+                {
+                    Console.WriteLine(item + " ");
+                }
+            }
+
+        }
+    }
+}
