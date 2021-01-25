a03682771b084cba24295cee361f64618990428a
Mazitova <77325891+Mazitova@users.noreply.github.com>
2021-01-25 09:28:22 +0300

    changed

--- Laboratories320/Mazitova/Pr6/GameCreationStart.cs
+++ Laboratories320/Mazitova/Pr6/GameCreationStart.cs
@@ -6,5 +6,20 @@ namespace Laboratories320.Mazitova.Pr6
 {
     class GameCreationStart
     {
+        private string UserName { get; set; }
+        private DateTime DayOfBirth { get; set; }
+        private int FinishedMatches { get; set; }
+        private int Victories { get; set; }
+        private int GamePoints { get; set; }
+
+        public string SearchByName()
+        {
+            return "";
+        }
+
+        public void GameStat()
+        {
+
+        }
     }
 }
