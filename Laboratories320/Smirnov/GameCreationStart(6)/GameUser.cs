1fca910266eb2249cb0fa6bcd9b4ffc36989e69a
chestnut <alexandrrrrrr0702@gmail.com>
2021-01-25 09:29:00 +0300

    end of lesson 25.01.21

---
+++ Laboratories320/Smirnov/GameCreationStart(6)/GameUser.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Laboratories320.Smirnov.GameCreationStart_6_
+{
+    class GameUser
+    {
+        public string Name { get; private set; }
+        public DateTime DateOfBirth { get; private set; }
+        public int FinishedMatches { get; set; }
+        public int Victories { get; set; }
+        public int GamePoints { get; set; }
+
+        public GameUser(string name, DateTime dateofbirth, int finishedmatches, int victories, int gamepoints)
+        {
+            Name = name;
+            DateOfBirth = dateofbirth;
+            FinishedMatches = finishedmatches;
+            Victories = victories;
+            GamePoints = gamepoints;
+        }
+
+        public static void GameStat(GameUser gameUser)
+        {
+            Console.WriteLine($"User name: {gameUser.Name} \nDay of birth: {gameUser.DateOfBirth} \n" +
+                $"Finished matches: {gameUser.FinishedMatches} \nVictories: {gameUser.Victories} \nGame points: " +
+                $"{gameUser.GamePoints}");
+        }
+    }
+}
