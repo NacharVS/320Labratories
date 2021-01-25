4e1391066ea93472940e30633d7c71a08748df9c
Marsel011 <77325786+Marsel011@users.noreply.github.com>
2021-01-25 09:25:04 +0300

    End lesson 25.01.2021

--- Laboratories320/Musin/GameCreatStart/GameCreationStart.cs
+++ Laboratories320/Musin/GameCreatStart/GameCreationStart.cs
@@ -6,5 +6,19 @@ namespace Laboratories320.Musin.GameCreatStart
 {
     class GameCreationStart
     {
+        public string UserName { get; set; }
+        public DateTime DayOfBirth { get; set; }
+        public int FinishedMatches { get; set; }
+        public int Victories { get; set; }
+        public int GamePoints { get; set; }
+
+        public GameCreationStart(string UserName, DateTime DayOfBirth, int FinishedMatches, int Victories, int GamePoints)
+        {
+            this.UserName = UserName;
+            this.DayOfBirth = DayOfBirth;
+            this.FinishedMatches = FinishedMatches;
+            this.Victories = Victories;
+            this.GamePoints = GamePoints;
+        }
     }
 }
