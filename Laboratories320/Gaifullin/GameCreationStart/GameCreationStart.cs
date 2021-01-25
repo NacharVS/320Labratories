5df00f3a12267f2c58d697be65210c2b12e85807
@GGRustem <gaifullin.rustem2018@gmail.com>
2021-01-25 14:54:07 +0300

    Gaifullin
    25.01.21 commit

--- Laboratories320/Gaifullin/GameCreationStart/GameCreationStart.cs
+++ Laboratories320/Gaifullin/GameCreationStart/GameCreationStart.cs
@@ -6,6 +6,26 @@ namespace Laboratories320.Gaifullin.GameCreationStart
 {
     class GameCreationStart
     {
+        public string UserName { get; set; }
+        public DateTime DayOfBirth { get; set; }
+        public int FinishedMatches { get; set; }
+        public int Victories { get; set; }
+        public int GamePoints { get; set; }
+
+        public GameCreationStart(string name, DateTime BirthDate, int finishedMatches, int victories, int gamePoints)
+        {
+            this.UserName = name;
+            this.DayOfBirth = BirthDate;
+            this.FinishedMatches = finishedMatches;
+            this.Victories = victories;
+            this.GamePoints = gamePoints;
+        }
+
+        public int GetStat()
+        {
+            return FinishedMatches;
+        }
 
     }
 }
+ 
---
+++ Laboratories320/Gaifullin/GameCreationStart/PlayGround.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Laboratories320.Gaifullin.GameCreationStart
+{
+    class PlayGround
+    {
+        public static GameCreationStart[] gamers;
+
+        public static void Gamers()
+        {
+            GameCreationStart user1 = new GameCreationStart("Robby", new DateTime(11, 11, 11), 11,11, 11);
+            GameCreationStart user2 = new GameCreationStart("Miguel", new DateTime(05, 05, 05), 05, 05, 05);
+            GameCreationStart user3 = new GameCreationStart("Hawk", new DateTime(07, 07, 07), 07, 07, 07);
+            gamers = new GameCreationStart[] { user1, user2, user3 };
+        }
+
+        public static void SearchByName(string Name)
+        {
+            for (int i = 0; i < gamers.Length; i++)
+            {
+
+            }
+        }
+    }
+}
