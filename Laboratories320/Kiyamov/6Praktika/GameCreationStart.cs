8c589a98e503611cca6d29d63f19fd9f39ea3f9a
Kiyamov <77325623+Kiyamov@users.noreply.github.com>
2021-01-27 01:56:13 +0300

    F

--- Laboratories320/Kiyamov/6Praktika/GameCreationStart.cs
+++ Laboratories320/Kiyamov/6Praktika/GameCreationStart.cs
@@ -1,38 +1,56 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-using System.Threading;
+using System.Threading.Tasks;
 using Laboratories320.Kiyamov._6Praktika;
 
 namespace Laboratories320.Kiyamov._6Praktika
 {
-    class GameCreationStart
+    class Player
     {
         public string UserName { get; set; }
-        public DateTime DayOfBirth { get; set; }
+        public int DayOfBirth { get; set; }
         public int Victories { get; set; }
         public int GamePoints { get; set; }
         public int FinishedMatches { get; set; }
+
+
+        public Player(string UserName, int DayOfBirth, int Victories, int GamePoints, int FinishedMatches)
+        {
+            this.UserName = UserName;
+            this.DayOfBirth = DayOfBirth;
+            this.Victories = Victories;
+            this.GamePoints = GamePoints;
+            this.FinishedMatches = FinishedMatches;
+
+        }
+        public void Search(string name)
+        {
+            if (name == UserName)
+            {
+                Console.WriteLine($": {UserName}");
+            }
+        }
+    }
+        class CreatePlayerStart
+        {
+            static Player Marsel = new Player("Marsel", 2001, 19, 12, 1726);
+            static Player Ilham = new Player("Ilham", 2004, 9, 9, 2972);
+            static Player Kamil = new Player("Kamil", 1859, 5, 7, 5145);
+            static List<Player> players = new List<Player> { Marsel, Ilham, Kamil };
+            public static void SearchByName(string UserName)
+            {
+                Parallel.ForEach(players, s => s.Search(UserName));
+
+            }
+        }
+    }
+
+
+    
+
+        
+      
     
 
 
-    public GameCreationStart(string UserName, DateTime DayOfBirth, int Victories, int GamePoints, int FinishedMatches)
-    {
-        this.UserName = UserName;
-        this.DayOfBirth = DayOfBirth;
-        this.Victories = Victories;
-        this.GamePoints = GamePoints;
-        this.FinishedMatches = FinishedMatches;
-
-
-        }
-    public class Player
-    {
-        Player Player1 = new ("", new DateTime(2000, 2, 4), 28, 25, 75);
-
-
-
-        }
-
-
-}
