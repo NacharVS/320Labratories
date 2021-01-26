49caa557874611f782783ab905954958b256d812
Kiyamov <77325623+Kiyamov@users.noreply.github.com>
2021-01-27 01:18:58 +0300

    процесс

--- Laboratories320/Kiyamov/6Praktika/GameCreationStart.cs
+++ Laboratories320/Kiyamov/6Praktika/GameCreationStart.cs
@@ -11,7 +11,28 @@ namespace Laboratories320.Kiyamov._6Praktika
         public string UserName { get; set; }
         public DateTime DayOfBirth { get; set; }
         public int Victories { get; set; }
-        public int GamePoints{ get; set; }
+        public int GamePoints { get; set; }
         public int FinishedMatches { get; set; }
-    }
+    
+
+
+    public GameCreationStart(string UserName, DateTime DayOfBirth, int Victories, int GamePoints, int FinishedMatches)
+    {
+        this.UserName = UserName;
+        this.DayOfBirth = DayOfBirth;
+        this.Victories = Victories;
+        this.GamePoints = GamePoints;
+        this.FinishedMatches = FinishedMatches;
+
+
+        }
+    public class Player
+    {
+        Player Player1 = new ("", new DateTime(2000, 2, 4), 28, 25, 75);
+
+
+
+        }
+
+
 }
