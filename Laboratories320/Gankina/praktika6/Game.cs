877d39aac7a0e08df0967f3cfea6f089343faf9a
Gankina <mari.gankina@bk.ru>
2021-01-26 22:53:23 +0300

    Сontinuation Gankina praktika6 26.01

--- Laboratories320/Gankina/praktika6/Game.cs
+++ Laboratories320/Gankina/praktika6/Game.cs
@@ -1,16 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Laboratories320.Gankina.praktika6
 {
     class Game
     {
-        public string UserName;
-        public DateTime Birthday;
-        public string CompletedMatches;
-        public string Wins;
-        public string GamePoints;
+        private string userName;
+        public DateTime birthday;
+        public int completedMatches;
+        public int wins;
+        public int gamePoints;
+
+        List<Game> users = new List<Game> { };
+        public string UserName { get; set; }
+        public DateTime Birthday { get; set; }
+        public int CompletedMatches { get; set; }
+        public int Wins { get; set; }
+        public int GamePoints { get; set; }
+
 
 
     }
