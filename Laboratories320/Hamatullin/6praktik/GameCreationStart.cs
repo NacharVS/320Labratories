1661389782569d1a89829f79a66bdce2666960bb
Hamich <ihamatullin@gmail.com>
2021-01-26 15:26:18 +0300

    finish

--- Laboratories320/Hamatullin/6praktik/GameCreationStart.cs
+++ Laboratories320/Hamatullin/6praktik/GameCreationStart.cs
@@ -38,7 +38,8 @@ namespace Laboratories320.Hamatullin._6praktik
         static Player player1 = new Player("Vasyan", 2002, 10, 5, 1000);
         static Player player2 = new Player("Imilkin", 2000, 29, 10, 3203);
         static Player player3 = new Player("KiamchikKruts", 1999, 50, 30, 7203);
-        static List<Player> players = new List<Player> { player1, player2, player3 };
+        static Player player4 = new Player("AzatStank", 1989, 60, 40, 8203);
+        static List<Player> players = new List<Player> { player1, player2, player3, player4 };
         public static void SearchByName(string name)
         {
             Parallel.ForEach(players, s => s.Search(name));
