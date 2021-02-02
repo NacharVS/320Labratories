ce663e2037745f85070b0a2e395dfe3760e5815b
keston616 <airat.shigapov2015@yandex.ru>
2021-02-02 15:07:26 +0300

    EndLesson(02/02/21)

---
+++ Laboratories320/Shigapov/Mongo/MongpTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Laboratories320.Shigapov.StrategyGame;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+namespace Laboratories320.Shigapov.Mongo
+{
+    class MongpTest
+    {
+
+    }
+}
