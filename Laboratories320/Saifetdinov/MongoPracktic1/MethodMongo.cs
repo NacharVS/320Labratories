91f6895025351ffa3f9cd1b7fdccf60ec559252b
Эмиль <77325950+ImilSaifetdinov@users.noreply.github.com>
2021-02-02 15:06:56 +0300

    start and end lesson

---
+++ Laboratories320/Saifetdinov/MongoPracktic1/MethodMongo.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB;
+namespace Laboratories320.Saifetdinov.MongoPracktic1
+{
+    class MethodMongo
+    {
+    }
+}
