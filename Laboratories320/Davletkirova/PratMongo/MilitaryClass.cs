a577379cfaddf3313790236dfed75f40785f6596
Chergionva <daryacherginova19@mail.ru>
2021-02-04 12:56:25 +0300

    finish

---
+++ Laboratories320/Davletkirova/PratMongo/MilitaryClass.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Driver;
+using MongoDB.Bson;
+
+namespace Laboratories320.Cherginova.PratMongo
+{
+    class MilitaryClass
+    {
+        public static async Task MongoInsert(Military military)
+        {
+            string connectionString = "mongodb://localhost:27017";
+            var client = new MongoClient(connectionString);
+            var database = client.GetDatabase("StrategyGame");
+            var collection = database.GetCollection<Military>("Military");
+            await collection.InsertOneAsync(military);
+        }
+
+        public static async Task SearchByName(string searchName)
+        {
+            string connectionString = "mongodb://localhost:27017";
+            var client = new MongoClient(connectionString);
+            var database = client.GetDatabase("StrategyGame");
+            var collection = database.GetCollection<Military>("Military");
+            var military = await collection.Find(std => std.Name == searchName).ToListAsync();
+
+            foreach (var item in military)
+            {
+                Console.WriteLine(item.Name);
+            }
+        }
+        public static async Task MongoConnect()
+        {
+            string connectionString = "mongodb://localhost:27017";
+            var client = new MongoClient(connectionString);
+            var database = client.GetDatabase("StrategyGame");
+            var collection = database.GetCollection<Military>("Military");
+            var military = new BsonDocument();
+            var militaries = await collection.Find(military).ToListAsync();
+
+            foreach (var item in military)
+            {
+                Console.WriteLine(item.Name);
+
+            }
+
+        }
+    }
+}
