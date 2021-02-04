722107fc537a9a286001a358c5e5f20e9779fe4d
Valieva <guz.valieva122@gmail.com>
2021-02-04 09:09:06 +0300

    start 04.02

--- Laboratories320/Lab1/Hero.cs
+++ Laboratories320/Lab1/Hero.cs
@@ -1,14 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Driver;
+using System.Threading.Tasks;
 
 namespace Laboratories320
 {
     class Hero : IMovable, IDestroyable
     {
+        [BsonId]
+        public int HeroId;
+        [BsonIgnoreIfNull]
         public string Name { get; set; }
+        [BsonIgnoreIfDefault]
         public double Cost { get; set; }
+        [BsonIgnoreIfDefault]
         public int NumberOfLife { get; set; }
+        
 
         public void Move()
         {
@@ -20,4 +29,11 @@ namespace Laboratories320
 
         }
     }
+
+    static async Task HeroInsert(Hero h)
+    {
+        string connectionString = "mongo"
+    }
+
+
 }
