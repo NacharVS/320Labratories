a726d9912b1f523af1cb1a069480a48b59837aef
Elizkins <semenovaeliza7@gmail.com>
2021-02-04 09:12:10 +0300

    Update 04.02.21

--- Laboratories320/task 02.02.21/MongoDB.cs
+++ Laboratories320/task 02.02.21/MongoDB.cs
@@ -1,12 +1,49 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 using Laboratories320.task_14._01._21;
+using MongoDB.Bson;
+using MongoDB.Driver;
 
 namespace Laboratories320.task_02._02._21
 {
     class MongoDB
     {
-        Fairy fairy = new Fairy() {Name = "Bloom", Helth = 67, Kindness = 78, Knowlege = 84, Power = 45, SpesialPower = "Dragon Fire" };
+        //Fairy fairy = new Fairy() {Name = "Bloom", Helth = 67, Kindness = 78, Knowlege = 84, Power = 45, SpesialPower = "Dragon Fire" };
+
+        static async Task MongoInsert(Fairy fairy)
+        {
+            var database = new MongoClient("mongodb://localhost").GetDatabase("WarOfHarmony");
+            var collection = database.GetCollection<Fairy>("Alphea");
+            await collection.InsertOneAsync(fairy);
+        }
+
+
+        static async Task MongoConnect()
+        {
+            var database = new MongoClient("mongodb://localhost").GetDatabase("WarOfHarmony");
+            var collection = database.GetCollection<Fairy>("Alphea");
+            var fairy = new BsonDocument();
+            var fairis = await collection.Find(fairy).ToListAsync();
+            foreach (var item in fairis)
+            {
+                Console.WriteLine(item.Name);
+
+            }
+        }
+
+        static async Task SearchByName(string searchName)
+        {
+            var database = new MongoClient("mongodb://localhost").GetDatabase("WarOfHarmony");
+            var collection = database.GetCollection<Fairy>("Alphea");
+            var fairis = await collection.Find(fair => fair.Name == searchName).ToListAsync();
+            foreach (var item in fairis)
+            {
+                Console.WriteLine(item.SpesialPower);
+
+            }
+
+        }
     }
 }
--- Laboratories320/task 14.01.21/Fairy.cs
+++ Laboratories320/task 14.01.21/Fairy.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MongoDB.Bson;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,6 +7,7 @@ namespace Laboratories320.task_14._01._21
 {
     class Fairy : Unit, IHealth, IMove, IHeal
     {
+        public ObjectId Id { get; set; }
         public string SpesialPower { get; set; }
 
         public int Helth { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
