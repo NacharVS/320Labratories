6387c94914d872c520e6eebf352c672525a7f4ee
Mazitova <77325891+Mazitova@users.noreply.github.com>
2021-02-04 09:24:37 +0300

    changed the end of the lesson 04.02.21

---
+++ Laboratories320/Mazitova/Pr7/Hero.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Driver;
+
+
+namespace Laboratories320.Mazitova.Pr7
+{
+    class Hero
+    {
+        [BsonId]
+        public int HeroId;
+        [BsonIgnoreIfNull]
+        public string HeroName { get; set; }
+        [BsonIgnoreIfDefault]
+        public int Victories { get; set; }
+        [BsonIgnoreIfDefault]
+        private int GamePoints { get; set; }
+
+
+
+    }
+       
+}
