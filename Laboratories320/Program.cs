ce121a7924507cb9cc4d3a56ea05057db145e8de
@GGRustem <gaifullin.rustem2018@gmail.com>
2021-02-06 12:21:02 +0300

    Gaifullin
    Commit

--- Laboratories320/Program.cs
+++ Laboratories320/Program.cs
@@ -9,42 +9,11 @@ namespace Laboratories320.Gaifullin
 
         static void Main(string[] args)
         {
-            //Console.WriteLine("Leave hope behind all who enters here....");
-            //Console.WriteLine();
-
-            //int[] array = new int[20];
-            //Random rnd = new Random();
-            //for (int i = 0; i < 20; i++)
-            //{
-            //    array[i] = rnd.Next(1, 10);
-            //}
-            //Console.WriteLine("Generating");
-            //Console.WriteLine("[{0}]", string.Join(", ", array));
-            //Task task1 = Task.Run(() => PrTask.Сomposition(array));
-            //Task task2 = task1.ContinueWith(mul => PrTask.Even());
-            //task2.Wait();
-
-            //Parallel.Invoke(sum,nul,div);
+            
         }
-        //static void nul()
-        //{
-        //    Thread.Sleep(2000);
-        //    Console.WriteLine("1 Id: ",Task.CurrentId);
-        //    Console.WriteLine("Result:"+ 5*9);
 
-        //}
-        //static void div()
-        //{
-        //    Thread.Sleep(2000);
-        //    Console.WriteLine("2 Id: ", Task.CurrentId);
-        //    Console.WriteLine("Result:"+ 5/9);
-        //}
-        //static void sum()
-        //{
-        //    Thread.Sleep(2000);
-        //    Console.WriteLine("3 Id: ", Task.CurrentId);
-        //    Console.WriteLine("Result:",+ 5+9);
-        //}
+
+
 
     }
 }
