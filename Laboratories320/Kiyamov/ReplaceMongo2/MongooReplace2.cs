9b71a4354e3fc0ba1eae42178eb0719632dadd46
Kiyamov <77325623+Kiyamov@users.noreply.github.com>
2021-02-08 23:01:03 +0300

    Update MongooReplace2.cs

--- Laboratories320/Kiyamov/ReplaceMongo2/MongooReplace2.cs
+++ Laboratories320/Kiyamov/ReplaceMongo2/MongooReplace2.cs
@@ -1,10 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using Laboratories320.Kiyamov._1_Praktika;
+using System.Threading.Tasks;
 
 namespace Laboratories320.Kiyamov.ReplaceMongo2
 {
     class MongooReplace2
     {
+        public static async Task Replace1(Builder builder, string newNameBuilder)
+        {
+            string connectionString = "mongodb://localhost:27017";
+            var client = new MongoClient(connectionString);
+            var database = client.GetDatabase("MondoBuilderr");
+            var collection = database.GetCollection<Builder>("builder");
+            await collection.ReplaceOneAsync(builder => builder.Name == newNameBuilder, builder);
+
+
+        }
+        public static async Task Replace2(Builder builder, string newNameBuilder)
+        {
+            string connectionString = "mongodb://localhost:27017";
+            var client = new MongoClient(connectionString);
+            var database = client.GetDatabase("MondoBuilderr");
+            var collection = database.GetCollection<Builder>("builder");
+            await collection.ReplaceOneAsync(builder => builder.Name == newNameBuilder, builder, new ReplaceOptions { IsUpsert = true });
+        }
+
     }
 }
