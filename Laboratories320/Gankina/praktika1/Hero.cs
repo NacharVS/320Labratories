116ce80a1e7e06797afe84f88443495d0f1c3603
Valieva <guz.valieva122@gmail.com>
2021-02-08 08:53:41 +0300

    Lesson start 08.02

--- Laboratories320/Gankina/praktika1/Hero.cs
+++ Laboratories320/Gankina/praktika1/Hero.cs
@@ -35,6 +35,36 @@ namespace Laboratories320
             var database = client.GetDatabase("Milka");
             var collection = database.GetCollection<Hero>("Hero");
             await collection.InsertOneAsync(h);
+
         }
+        public static async Task ReplaceByName(string newname, Hero newhero)
+        {
+            string connectionString = "mongodb://localhost";
+            var client = new MongoClient(connectionString);
+            var database = client.GetDatabase("Milka");
+            var collection = database.GetCollection<Hero>("Hero");
+            await collection.ReplaceOneAsync(hero => hero.Name == newname, newhero);
+        }
+
+        public static async Task Update(string name, int numberoflife)
+        {
+            string connectionString = "mongodb://localhost";
+            var client = new MongoClient(connectionString);
+            var database = client.GetDatabase("Milka");
+            var collection = database.GetCollection<Hero>("Hero");
+            var update = Builders<Hero>.Update.Set(h => h.NumberOfLife, numberoflife);
+            await collection.UpdateOneAsync(hero => hero.Name == name, update);
+        }
+
+        public static async Task UpdateMany(string name, int numberoflife)
+        {
+            string connectionString = "mongodb://localhost";
+            var client = new MongoClient(connectionString);
+            var database = client.GetDatabase("Milka");
+            var collection = database.GetCollection<Hero>("Hero");
+            var update = Builders<Hero>.Update.Set(h => h.NumberOfLife, numberoflife);
+            await collection.UpdateManyAsync(hero => hero.Name == name, update);
+        }
+
     }
 }
