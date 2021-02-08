be31a661f93c2997a80c8718d117b41a50f731d2
evgenvolkov177 <77325711+evgenvolkov177@users.noreply.github.com>
2021-02-09 01:17:28 +0300

    FinishSecondPracticeOfMongo

---
+++ Laboratories320/Volkov/MongoSecondPractice/MethodReplace.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Driver;
+using MongoDB.Bson;
+using Laboratories320.Volkov._1_practice;
+using Laboratories320.Volkov._1_practice.War_Units;
+
+namespace Laboratories320.Volkov.MongoSecondPractice
+{
+    class MethodReplace
+    {
+        public static async Task ReplaceMongo(Archer archer, string nameArcher)
+        {
+            string connectionString = "mongodb://localhost:27017";
+            var client = new MongoClient(connectionString);
+            var database = client.GetDatabase("Game");
+            var collection = database.GetCollection<Archer>("Archers");
+            await collection.ReplaceOneAsync(archer => archer.Name == nameArcher, archer);
+        }
+        public static async Task ReplaceMongoUpsert(Archer archer, string nameArcher)
+        {
+            string connectionString = "mongodb://localhost:27017";
+            var client = new MongoClient(connectionString);
+            var database = client.GetDatabase("Game");
+            var collection = database.GetCollection<Archer>("Archers");
+            await collection.ReplaceOneAsync(archer => archer.Name == nameArcher, archer, new ReplaceOptions { IsUpsert = true });
+        }
+    }
+}
