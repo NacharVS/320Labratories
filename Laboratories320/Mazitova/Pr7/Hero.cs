a3ede702d2b90dd9d85adb7774c61616ca51af6d
Mazitova <77325891+Mazitova@users.noreply.github.com>
2021-02-08 09:03:54 +0300

    finish 8.02.21 (continued Pr7)

--- Laboratories320/Mazitova/Pr7/Hero.cs
+++ Laboratories320/Mazitova/Pr7/Hero.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 
 namespace Laboratories320
 {
-    class Hero 
+    class Hero
     {
         [BsonId(IdGenerator = typeof(StringObjectIdGenerator))]
         [BsonIgnoreIfDefault]
@@ -17,7 +17,7 @@ namespace Laboratories320
         [BsonIgnoreIfNull]
         public string HeroName { get; set; }
         [BsonIgnoreIfDefault]
-        public int Health { get; set; } 
+        public int Health { get; set; }
 
         public static async Task HeroInsert(Hero h)
         {
@@ -27,7 +27,7 @@ namespace Laboratories320
             var collection = database.GetCollection<Hero>("Hero");
             await collection.InsertOneAsync(h);
         }
-        public static async Task HeroNameResault (string heroName, Hero newHero)
+        public static async Task HeroNameResault(string heroName, Hero newHero)
         {
             string connectionString = "mongodb://localhost";
             var client = new MongoClient(connectionString);
@@ -36,5 +36,41 @@ namespace Laboratories320
             await collection.ReplaceOneAsync(hero => hero.HeroName == heroName, newHero);
         }
 
+        public static async Task UpdateMany(string heroName, int newHealth)
+        {
+            string connectionString = "mongodb://localhost";
+            var client = new MongoClient(connectionString);
+            var database = client.GetDatabase("Teddy");
+            var collection = database.GetCollection<Hero>("Hero");
+            var update = Builders<Hero>.Update.Set(hero => hero.Health, newHealth);
+            await collection.UpdateManyAsync(hero => hero.HeroName == heroName, update);
+        }
+        public static async Task DeleteMany(string heroName, int newHealth)
+        {
+            string connectionString = "mongodb://localhost";
+            var client = new MongoClient(connectionString);
+            var database = client.GetDatabase("Teddy");
+            var collection = database.GetCollection<Hero>("Hero");
+            var delete = Builders<Hero>.Filter.Eq(hero => hero.Health, newHealth);
+            await collection.DeleteManyAsync(delete);
+        }
+        public static async Task Update(string heroName, int newHealth)
+        {
+            string connectionString = "mongodb://localhost";
+            var client = new MongoClient(connectionString);
+            var database = client.GetDatabase("Teddy");
+            var collection = database.GetCollection<Hero>("Hero");
+            var update = Builders<Hero>.Update.Set(hero => hero.Health, newHealth);
+            await collection.UpdateOneAsync(hero => hero.HeroName == heroName, update);
+        }
+        public static async Task Delete(string heroName, int newHealth)
+        {
+            string connectionString = "mongodb://localhost";
+            var client = new MongoClient(connectionString);
+            var database = client.GetDatabase("Teddy");
+            var collection = database.GetCollection<Hero>("Hero");
+            var delete = Builders<Hero>.Filter.Eq(hero => hero.Health, newHealth);
+            await collection.DeleteOneAsync(delete);
+        }
     }
 }
