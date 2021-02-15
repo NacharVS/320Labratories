d3e9aca4620ac3736d965bde0c18451f8c72488b
Chergionva <daryacherginova19@mail.ru>
2021-02-15 09:23:14 +0300

    finish GameCreationStart

--- Laboratories320/Davletkirova/GameCreationStart/User.cs
+++ Laboratories320/Davletkirova/GameCreationStart/User.cs
@@ -1,23 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
 
 namespace Laboratories320.Davletkirova.GameCreationStart
 {
     class User
     {
-        public string Name { get; set; }
-        public DateTime DayOfBirth { get; set; }
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string DayOfBirth { get; set; }
         public int FinishedMatches { get; set; }
         public int Victories { get; set; }
-        public double GamePoints { get; set; }
-        public string SearchByName(string s)
+        public int GamePoints { get; set; }
+
+        public User(int id, string userName, string dayOfBirth, int finishedMatchees, int victories, int gamePoints)
         {
-            return s;
+            this.Id = id;
+            this.UserName = userName;
+            this.DayOfBirth = dayOfBirth;
+            this.FinishedMatches = finishedMatchees;
+            this.Victories = victories;
+            this.GamePoints = gamePoints;
         }
-        public int GameStat(int i)
+        public void GetAName()
         {
-            return i;
+            Console.WriteLine($"Id player - {Id} name player - {UserName}");
+        }
+        public void StatisticsGame()
+        {
+            Console.WriteLine($"Id player - {Id}, FinishedMatches - {FinishedMatches}, Victories - {Victories}, Game Points - {GamePoints}");
         }
     }
 }
