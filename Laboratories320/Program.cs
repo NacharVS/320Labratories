c7f32477e0d8b198495ee29f5777cbc9be993ff5
keston616 <airat.shigapov2015@yandex.ru>
2021-05-03 09:56:18 +0300

    Json Task

--- Laboratories320/Program.cs
+++ Laboratories320/Program.cs
@@ -8,6 +8,10 @@ using Laboratories320.Shigapov.ContinuationTasks;
 using Laboratories320.Shigapov.GameCreationStart;
 using Laboratories320.Shigapov.StrategyGame;
 using Laboratories320.Shigapov.Mongo;
+using Laboratories320.Shigapov.Json;
+using System.IO;
+using System.Text.Json;
+using System.Collections.Generic;
 
 namespace Laboratories320
 {
@@ -30,6 +34,41 @@ namespace Laboratories320
             //Task task2 = task1.ContinueWith(mul => Continuation.Even());
             //task2.Wait();
 
+            //Practic Json
+            Student stud1 = new Student("Alex", 228, 18);
+            Student stud2 = new Student("Maria", 229, 18);
+            Student stud3 = new Student("Rail", 228, 19);
+            List<Student> st = new List<Student> { stud1, stud2, stud3 };
+
+            string path = @"D:\Проекты С#\Json\Json\bin\Debug\note.txt";
+            using (StreamWriter streamWriter = new StreamWriter(path, false, System.Text.Encoding.Default))
+            {
+                foreach (var item in st)
+                {
+                    streamWriter.WriteLine(JsonSerializer.Serialize<Student>(item));
+                }
+            }
+
+            List<string> studentRead = new List<string>();
+            using (StreamReader sr = new StreamReader(path, System.Text.Encoding.Default))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    studentRead.Add(line);
+                }
+            }
+
+            foreach (var item in studentRead)
+            {
+                Student s = JsonSerializer.Deserialize<Student>(item);
+                if (s.Name == "Alex")
+                {
+                    Console.WriteLine("Name: " + s.Name);
+                    Console.WriteLine("Age: " + s.Age);
+                    Console.WriteLine("Group: " + s.Group);
+                }
+            }
         }
     }
 }
