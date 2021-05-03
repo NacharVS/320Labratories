d4046eb17148b255908f5cfac1527b4d0907dde7
goosberry <saetova_i@list.ru>
2021-05-03 08:20:07 +0300

    Create Student.cs

---
+++ Laboratories320/json/Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestJson
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Group { get; set; }
+    }
+
+}
