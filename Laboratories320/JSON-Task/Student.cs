f8a09ac5e5a2c9d553caa3865095d061d3e68255
Elizkins <semenovaeliza7@gmail.com>
2021-05-03 08:13:24 +0300

    json-task

---
+++ Laboratories320/JSON-Task/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Laboratories320
+{
+    class Student
+    {
+        private static int idCounter = 0;
+        public int Id { get; private set; } = ++idCounter;
+        public string Name { get; set; }
+        public int Group { get; set; }
+
+        public override string ToString()
+        {
+            return $"-------------------------------\n\tId: {Id}\n\tName: {Name}\n\tGroup: {Group}\n-------------------------------\n";
+        }
+    }
+}
--- Laboratories320/Program.cs
+++ Laboratories320/Program.cs
@@ -1,9 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
 using System.Threading;
 using System.Threading.Tasks;
-using Laboratories320.task_02._02._21;
-using Laboratories320.task_14._01._21;
 
 namespace Laboratories320
 {
@@ -11,7 +12,66 @@ namespace Laboratories320
     {
         static void Main(string[] args)
         {
-            MongoDBTest.MainMongo();
+            Student s1 = new Student
+            {
+                Name = "Alex",
+                Group = 230
+            };
+            Student s2 = new Student
+            {
+                Name = "Daniil",
+                Group = 322
+            };
+            Student s3 = new Student
+            {
+                Name = "Hainz",
+                Group = 115
+            };
+            Student s4 = new Student
+            {
+                Name = "Potap",
+                Group = 205
+            };
+            Student s5 = new Student
+            {
+                Name = "Ctepan",
+                Group = 410
+            };
+
+            List<Student> studentList = new List<Student>
+            {
+                s1, s2, s3, s4, s5
+            };
+            List<string> studentListInJson = new List<string>();
+
+            //запись в файл
+            using (StreamWriter sw = new StreamWriter("allStudents.txt", false, System.Text.Encoding.Default))
+            {
+                foreach (var item in studentList)
+                {
+                    sw.WriteLine(JsonSerializer.Serialize<Student>(item));
+                }
+            }
+
+            //чтение из файла
+            using (StreamReader sr = new StreamReader("allStudents.txt", System.Text.Encoding.Default))
+            {
+                string student;
+                while ((student = sr.ReadLine()) != null)
+                {
+                    studentListInJson.Add(student);
+                }
+            }
+
+            studentList.Clear();
+
+            foreach (var item in studentListInJson)
+            {
+                studentList.Add(JsonSerializer.Deserialize<Student>(item));
+            }
+
+            Console.WriteLine($"Данные о студенте с именем {s1.Name}");
+            Console.WriteLine(studentList.Where(s => s.Name == s1.Name).First().ToString());
         }
     }
 }
