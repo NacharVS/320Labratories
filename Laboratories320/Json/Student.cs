f00ff27ed04a3ca9e71554eaecb5a0c95efd9de1
evgenvolkov177 <77325711+evgenvolkov177@users.noreply.github.com>
2021-05-07 21:52:35 +0300

    Json 07.05.2021

---
+++ Laboratories320/Json/Student.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Laboratories320.Json
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public int NumberGroup { get; set; }
+    }
+}
--- Laboratories320/Program.cs
+++ Laboratories320/Program.cs
@@ -4,7 +4,9 @@ using System.Threading;
 using Laboratories320.Volkov.ThreadLocker;
 using Laboratories320.Volkov.Tasks;
 using Laboratories320.Volkov.TasksPractice2;
-
+using Laboratories320.Json;
+using System.IO;
+using System.Text.Json;
 
 namespace Laboratories320
 {
@@ -12,8 +14,53 @@ namespace Laboratories320
     {
         static void Main(string[] args)
         {
-            TaskPractice2.Work();
+            string path = @"C:\Users\evgen\Desktop\Text.txt";
+
+            Student[] studentInfos =
+            {
+                new Student { Name = "Евгений", NumberGroup = 320, Age = 19},
+                new Student { Name = "Раиль", NumberGroup = 320, Age = 19},
+                new Student { Name = "Катя", NumberGroup = 124, Age = 16},
+                new Student { Name = "Алена", NumberGroup = 124, Age = 16},
+                new Student { Name = "Илина", NumberGroup = 320, Age = 19},
+                new Student { Name = "Даниил", NumberGroup = 220, Age = 18},
+                new Student { Name = "Света", NumberGroup = 323, Age = 18},
+            };
+
+            string stringJson = "";
+
+
+            using (FileStream fs = File.Create(path))
+            {
+                foreach (var item in studentInfos)
+                {
+                    stringJson += $"{JsonSerializer.Serialize(item)} \n";
+                }
+            };
+
+            StreamWriter streamWriter = new StreamWriter(path);
+            streamWriter.WriteLine(stringJson);
+            streamWriter.Close();
+
+            StreamReader streamreader = new StreamReader(path);
+            string line = streamreader.ReadLine();
+
+            Console.WriteLine("Введите имя студента - ");
+            string nameStudent = Console.ReadLine();
+
+            while (line != null)
+            {
+                Student rStudent = JsonSerializer.Deserialize<Student>(line);
+                if (rStudent.Name == nameStudent)
+                {
+                    string json2 = JsonSerializer.Serialize<Student>(rStudent);
+                    Console.WriteLine(json2);
+                    break;
+                }
+                line = streamreader.ReadLine();
+            }
+            Console.ReadKey();
         }
-        
+
     }
 }
